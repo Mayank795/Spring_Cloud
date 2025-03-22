9bb603521c492cd9a3251a2871f9982272e5f794
Mayank795 <mayankspal555@gmail.com>
2025-03-22 12:59:38 +0530

    adding files

---
+++ question-service/src/main/java/com/telusko/question_service/dao/QuestionDao.java
@@ -0,0 +1,20 @@
+package com.telusko.question_service.dao;
+
+
+import com.telusko.question_service.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    //find question by category
+    List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT q.id FROM question q WHERE q.category =:category ORDER BY RAND() LIMIT :numQuestions", nativeQuery = true)
+    List<Integer> findRandomQuestionsByCategory(@Param("category") String category, @Param("numQuestions") Integer numQuestions);
+}
